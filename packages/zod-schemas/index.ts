f9d9002b538de3e335561d503425bedddb18a6e9
Shashank-Salian <shashanksalian96@gmail.com>
2025-07-15 18:05:11 +0530

    forgot about this fella

--- packages/zod-schemas/index.ts
+++ packages/zod-schemas/index.ts
@@ -1,4 +1,4 @@
 export * from "./src/categories";
-export * from "./src/users";
-export * from "./src/products";
 export * from "./src/order-items";
+export * from "./src/products";
+export * from "./src/users";
